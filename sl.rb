968ab75c6230ed82825902692c6c0c0eccf84f71
hfu <hfu@fkmac1.local>
2015-07-22 12:05:10 +0900

    Update sl.rb and README.md

--- sl.rb
+++ sl.rb
@@ -1,5 +1,44 @@
 require 'open-uri'
 require 'json'
+require 'time'
+require 'net/http'
+require 'action_view'
+
+I18n.enforce_available_locales = false
+$av = ActionView::Base.new
+
+def name(entry, stack)
+  return unless entry['url'].include?('/{z}/{x}/{y}')
+  print "#{stack.join('::')}\n"
+end
+
+def mokuroku_urls(entry, stack)
+  r = entry['url'].split('/{z}/{x}/{y}')
+  return if r.size == 1
+  print "#{r[0]}/mokuroku.csv.gz\n"
+end
+
+def mokuroku_info(entry, stack)
+  r = entry['url'].split('/{z}/{x}/{y}')
+  return if r.size == 1
+  server = r[0].split('/')[2]
+  path = r[0].split(server)[1] + '/mokuroku.csv.gz'
+  proxy_host, proxy_port =
+    (ENV['http_proxy'] || '').sub(/http:\/\//, '').split(':')
+  Net::HTTP.Proxy(proxy_host, proxy_port).start(server) {|http|
+    resp = http.head(path)
+    s = [
+      resp.code == '200' ? '○' : '☓',
+      path.sub('/xyz/', '').sub('/mokuroku.csv.gz', '')
+    ]
+    s << [
+      Time.parse(resp['last-modified']).localtime.to_s.sub(' +0900', ''),
+      $av.number_to_human_size(resp['content-length'])
+    ] if resp.code == '200'
+    s << stack.join('/')
+    print s.join(','), "\n"
+  }
+end
 
 def process(entry, stack)
   case entry['type']
@@ -11,10 +50,17 @@ def process(entry, stack)
     stack.pop
   when 'Layer'
     stack.push(entry['title'])
-    print "#{stack.join('::')}\n"
+    $methods.each {|method|
+      send(method, entry, stack)
+    }
     stack.pop
   end
 end
+
+$methods = ARGV.map{|v| v.start_with?('--') ? v[2..-1].to_sym : nil}.compact
+$methods = [
+  :mokuroku_info
+] if $methods.size == 0
 
 [0, 1, 2, 3, 4, '_experimental'].each {|e|
   url = "http://gsi-cyberjapan.github.io/gsimaps/layers_txt/layers#{e}.txt"
